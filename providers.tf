5ef68dbb57735bd9f1da28662c6ab3c78158594f
Chris Hubbard <chris@thehubbards.org>
2021-04-09 16:52:01 -0400

    Specify aws provider version

--- providers.tf
+++ providers.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
+  version    = "~> 2.61"
 }
 
 provider "cloudflare" {
