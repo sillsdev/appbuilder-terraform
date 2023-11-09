34668ef8764019ebc69aebc6dd9e07d3312a702c
Chris Hubbard <chris@thehubbards.org>
2023-11-09 16:06:16 -0500

    Upgrade to v0.14.11

--- providers.tf
+++ providers.tf
@@ -2,12 +2,10 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
-  version    = "~> 2.61"
 }
 
 provider "cloudflare" {
-  email   = var.cloudflare_email
-  token   = var.cloudflare_token
-  version = "~> 1.0"
+  email = var.cloudflare_email
+  token = var.cloudflare_token
 }
 
