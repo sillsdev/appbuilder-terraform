19e8d92d112630a06318783ea8077af6603f1318
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2026-06-01 12:03:42 -0400

    Fix 0.0.0.0 ips

--- main.tf
+++ main.tf
@@ -143,7 +143,7 @@ resource "aws_security_group_rule" "limited_buildengine" {
   to_port           = 8443
   protocol          = "tcp"
   security_group_id = aws_security_group.alb_https_limited_ips.id
-  cidr_blocks       = concat(var.https_ips, ["${aws_eip.public.public_ip}/32"])
+  cidr_blocks       = contains(var.https_ips, "0.0.0.0/0") ? ["0.0.0.0/0"] : toset(concat(var.https_ips, ["${aws_eip.public.public_ip}/32"]))
 }
 
 // Create application load balancer for public access
