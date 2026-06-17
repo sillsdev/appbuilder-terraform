be6473f5106f5f6081d1d743716d4358f972f2ec
Chris Hubbard <chris@thehubbards.org>
2026-06-17 12:01:49 -0400

    Update sil terraform-modules to 7.2.0; clean up cloudflare provider

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 // Create VPC
 module "vpc" {
-  source    = "github.com/silinternational/terraform-modules//aws/vpc-public-only?ref=3.5.0"
+  source    = "github.com/silinternational/terraform-modules//aws/vpc-public-only?ref=7.2.0"
   app_name  = var.app_name
   app_env   = var.app_env
   aws_zones = var.aws_zones
@@ -8,7 +8,7 @@ module "vpc" {
 
 // Create ecs cluster
 module "ecscluster" {
-  source   = "github.com/silinternational/terraform-modules//aws/ecs/cluster?ref=3.5.0"
+  source   = "github.com/silinternational/terraform-modules//aws/ecs/cluster?ref=7.2.0"
   app_name = var.app_name
   app_env  = var.app_env
 }
@@ -96,7 +96,7 @@ resource "random_id" "buildengine_db_root_pass" {
 
 // Create DB
 module "rds" {
-  source                  = "github.com/silinternational/terraform-modules//aws/rds/mariadb?ref=3.5.0"
+  source                  = "github.com/silinternational/terraform-modules//aws/rds/mariadb?ref=7.2.0"
   app_name                = var.app_name
   app_env                 = var.app_env
   db_name                 = var.buildengine_db_name
@@ -170,7 +170,6 @@ resource "aws_instance" "ecshost" {
 }
 
 resource "aws_eip" "public" {
-  vpc      = true
   instance = aws_instance.ecshost.id
 
   tags = {
@@ -211,7 +210,7 @@ resource "aws_security_group_rule" "limited_dwkit" {
 
 // Create application load balancer for public access
 module "alb" {
-  source          = "github.com/silinternational/terraform-modules//aws/alb?ref=3.5.0"
+  source          = "github.com/silinternational/terraform-modules//aws/alb?ref=7.2.0"
   app_name        = var.app_name
   app_env         = var.app_env
   internal        = "false"
@@ -904,7 +903,7 @@ resource "aws_codebuild_project" "publish" {
 
 // Uses default target group to route all https/443 traffic to buildengine
 module "ecsservice_buildengine" {
-  source             = "github.com/silinternational/terraform-modules//aws/ecs/service-only?ref=3.5.0"
+  source             = "github.com/silinternational/terraform-modules//aws/ecs/service-only?ref=7.2.0"
   cluster_id         = module.ecscluster.ecs_cluster_id
   service_name       = "buildengine"
   service_env        = var.app_env
