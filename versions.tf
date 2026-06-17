699dee2b735f6188254e7ae31ec4863be2a08b7c
Chris Hubbard <chris@thehubbards.org>
2026-06-17 12:10:58 -0400

    Try Cloudflare provider 2.9 ????

--- versions.tf
+++ versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 2.0"
+      version = "~> 2.9"
     }
     random = {
       source = "hashicorp/random"
