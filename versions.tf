42635520ce2a0e15db9521957ca80918f0548f0f
Chris Hubbard <chris@thehubbards.org>
2026-06-17 11:18:33 -0400

    Upgrade to Terraform 0.13.7

--- versions.tf
+++ versions.tf
@@ -1,3 +1,15 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 }
